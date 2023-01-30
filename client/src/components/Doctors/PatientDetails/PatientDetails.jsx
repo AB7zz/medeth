e1ce6aaf7bd7babe4b6c9cfd6e2a8d8e1712de0e
harshedabdulla <harshed2004@gmail.com>
2023-01-30 16:14:48 +0530

    patientDetails

--- client/src/components/Doctors/PatientDetails/PatientDetails.jsx
+++ client/src/components/Doctors/PatientDetails/PatientDetails.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+import Medications from './Medications/Medications'
+import PatientBio from './PatientBio/PatientBio'
+import PrevAppointments from './PrevAppointments/PrevAppointments'
 
 const PatientDetails = () => {
   return (
-    <div>
-    <p>harshed</p>
-    </div>
+    <div style={{ display: 'flex'}}>
+    <Sidebar style={{ flex: 1 }} />
+    <Medications style={{ flex: 1 }} />
+    <PrevAppointments style={{ flex: 1 }} />
+    <PatientBio style={{flex:1}} />
+  </div>
   )
 }
 
--- client/src/components/Doctors/PatientDetails/PrevAppointments/PrevAppointments.jsx
+++ client/src/components/Doctors/PatientDetails/PrevAppointments/PrevAppointments.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const PrevAppointments = () => {
-  return (
-    <div>PrevAppointments</div>
-  )
-}
+  const [appointments, setAppointments] = useState([]);
 
-export default PrevAppointments
+  useEffect(() => {
+    // retrieve the data from the API or data source
+    // and set the state with the returned data
+
+    // example data for demonstration purposes
+    setAppointments([
+      { date: '01-01-2022', time: '10:00 AM', with: 'Dr. John Doe' },
+      { date: '02-01-2022', time: '11:00 AM', with: 'Dr. Jane Doe' },
+      { date: '03-01-2022', time: '12:00 PM', with: 'Dr. Bob Smith' },
+    ]);
+  }, []);
+
+  return (
+    <div className="w-1/4 m-3 rounded-lg overflow-hidden shadow-lg">
+      <div className="px-6 py-2 bg-blue-500 text-white">
+        <h1 className="text-2xl font-bold">Previous Appointments</h1>
+      </div>
+      <ul className="mt-2">
+        {appointments.map((appointment, index) => (
+          <li key={index} className="px-6 py-4 border-b border-gray-400">
+            Date: {appointment.date}
+            <br />
+            Time: {appointment.time}
+            <br />
+            With: {appointment.with}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PrevAppointments;
